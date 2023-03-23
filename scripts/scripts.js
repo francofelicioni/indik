848614b96a4818bf8b9dc334370a551a267f43b0
Franco Felicioni <fran.felicioni@gmail.com>
2023-03-23 21:06:50 +0100

    JS Header is hidden/shown when scrolled

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,25 @@
 //Burger Menu
-let burgerMenu = document.querySelector(".nav-bar__burger-menu");
-let navBar = document.querySelector(".nav-bar__container");
+// let burgerMenu = document.querySelector(".nav-bar__burger-menu");
+// let navBar = document.querySelector(".nav-bar__container");
 
-burgerMenu.addEventListener("click", () => {
-  navBar.classList.toggle("active");
-  burgerMenu.classList.toggle("animation");
-});
+// burgerMenu.addEventListener("click", () => {
+//   navBar.classList.toggle("active");
+//   burgerMenu.classList.toggle("animation");
+// });
 
 // Header is hidden/shown when scrolled
 let initialScroll = window.scrollY;
 
 window.addEventListener("scroll", function () {
   let newScroll = window.scrollY;
+  console.log(newScroll)
   let width = window.innerWidth;
+  console.log(newScroll)
+  
 
   if (width > 1000 && initialScroll >= newScroll) {
-    document.getElementById("header").style.top = "40px";
+    document.getElementById("header").style.top = "0px";
+    // document.getElementById("header").style.backgroundColor="blue"
   } else if (width > 1000 && initialScroll <= newScroll) {
     document.getElementById("header").style.top = "-120px";
   }
